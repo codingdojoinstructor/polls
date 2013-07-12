136cef07413ce2c629b1f40cdc244b0090a4032f
frisk <friskslalom6@Randalls-MacBook-Pro-2.local>
2013-07-12 07:39:26 -0700

    First Commit #6485274077252

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poll::Application.config.secret_token = '7c2c6ac0e398e3e5c4472db35647c99cf201132ab551e3fd5b63b34825ab6a507398062c6f784135973ddf7d3476af79fde6e7a0514c48cb8195f2f51f5a3891'
